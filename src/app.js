e1342d8e41aa5b85b75e7c2aa2920cbe009fc172
Henrique S.A <henriqueator@gmail.com>
2020-06-29 01:49:50 -0300

    Some style in the pages

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
+const path = require('path');
 const express = require('express');
 const adminRoutes = require('./routers/admin.router');
 const shopRoutes = require('./routers/shop.router');
 const bodyParser = require('body-parser');
 
+const publicPath = path.join(__dirname, '../public');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(publicPath));
 // Routers
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use((req, res) => res.status(404).send('<h1>page not found!</h1>'));
+app.use((req, res) => res.status(404).sendFile(path.join(__dirname, './views/404.html')));
 
 app.listen(3000);
