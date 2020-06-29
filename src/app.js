d271ac7415d65afe3d857985411b2df5f9c0a38d
Henrique S.A <henriqueator@gmail.com>
2020-06-29 00:34:36 -0300

    - Added path filter to admin routes - home and /add-product HTML in views

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routers
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res) => res.status(404).send('<h1>page not found!</h1>'));
 
--- src/routers/admin.router.js
+++ src/routers/admin.router.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send('<h1>Add a produt</h1>');
+  res.sendFile(path.join(__dirname, '../views/add-product.html'));
 });
 
 router.post('/product', (req, res, next) => {
