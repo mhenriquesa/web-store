eac06237189a42423969ec1735be38fdfd5bf644
Henrique S.A <henriqueator@gmail.com>
2020-07-03 15:36:14 -0300

    Html templates - added dinamyc content

--- src/routers/admin.router.js
+++ src/routers/admin.router.js
@@ -5,12 +5,15 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Gypsy Store - Add a product' });
+  res.render('add-product', {
+    pageTitle: 'Gypsy Store - Add a product',
+  });
 });
 
 router.post('/add-product', (req, res, next) => {
   products.push({ title: req.body.title });
   res.redirect('/');
+  console.log(products);
 });
 
 module.exports = {
--- src/routers/shop.router.js
+++ src/routers/shop.router.js
@@ -4,8 +4,13 @@ const express = require('express');
 const router = express.Router();
 const { adminData } = require('./admin.router');
 
+const products = adminData;
+
 router.get('/', (req, res, next) => {
-  res.render('shop', { pageTitle: 'Gypsy Store' });
+  res.render('shop', {
+    pageTitle: 'Gypsy Store',
+    products: products,
+  });
 });
 
 module.exports = router;
